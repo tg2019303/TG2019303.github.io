a07f789c414fcec22961c1a88c93a962f11c409f
Allan Chain <1599654796@qq.com>
2019-07-18 20:53:39 +0800

    better open map interaction

--- infoUtil.js
+++ infoUtil.js
@@ -6,14 +6,15 @@ function showInfo(data, hilite=-1){
   }else{
     sContent += data.University
   };
-  sContent += '<div class="open_map_container"><button onclick="toggleOpenMap()" id="open_map_trigger">打开地图</button><div id="open_maps">';
+  sContent += '<hr>'
+  sContent += '<button onclick="toggleOpenMap()" id="open_map_trigger">外部地图</button><div id="open_maps">';
   sContent += `<a href=bdapp://map/marker?location=${data.Lat},${data.Lng}&coord_type=bd09ll&title=${data.University}&src=andr.baidu.openAPIdemo> 百度地图 </a>`;
   sContent += `<a href="https://apis.map.qq.com/uri/v1/marker?marker=coord:${data.Lat},${data.Lng};title:${data.University};&referer=myapp"> 腾讯地图 </a>`
   sContent += `<a href="//uri.amap.com/marker?position=${data.Lng},${data.Lat}&name=${data.University}&src=mypage&coordinate=gaode&callnative=0"> 高德地图 </a>`
-  sContent +='<br/><p>注意：腾讯和高德的定位会有偏差，如要使用请自行调整</p>'
+  sContent +='<br/><p>注意：腾讯和高德的定位会有偏差，<br/>如要使用请自行调整。</p>'
   // $("#open_map_trigger").click();
-  sContent += '</div></div>'
-  sContent += '<ul onclick="clearInfoDiv()">';
+  sContent += '</div>'
+  sContent += '<div onclick="clearInfoDiv()"><ul>';
   data.Students.forEach(function(student, index){
     var studentInfo = student.split("@");
     var studentHTML = '<li class="info_li">' + studentInfo[0];
@@ -34,7 +35,7 @@ function showInfo(data, hilite=-1){
     }else{
       sContent += studentHTML
     }});
-  sContent += '</ul><p style="font-size: 12px; align: center"><br/>轻触关闭窗口</p></div>';
+  sContent += '</ul><p style="font-size: 12px; align: center"><br/>轻触关闭窗口</p></div></div>';
   map.setCenter(data.Marker.getPosition());
   sContent = $.parseHTML(sContent);
   $(sContent).appendTo("body")
