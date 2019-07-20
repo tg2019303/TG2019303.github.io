b3cf1678bc8f9dc3693f5746f0a72f1a502c2b6e
Allan Chain <1599654796@qq.com>
2019-07-20 08:28:38 +0800

    add auto zoom (and auto indent code)

--- mapUtil.js
+++ mapUtil.js
@@ -12,42 +12,44 @@ function initMap() {
   //向地图中添加比例尺控件
   var ctrl_sca = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
   map.addControl(ctrl_sca);
-  aerialView();
-  map.enableScrollWheelZoom(); 
+  map.centerAndZoom(new BMap.Point(119,35),6);//初始化地图，point为中心点，缩放级别为6
+  map.setMapStyleV2({styleId: 'a5fddb25f2312728e44a8dd938e24fd2'});
+  map.enableScrollWheelZoom();
   //新建标注
   index_list=Array.from({length:universities.length}).map((item,index)=>{
-     return index;
-    });
+    return index;
+  });
   universities.forEach(setMarker);
   setInterval(function(){map.clearOverlays();randomOrder();universities.forEach(setMarker)}, 5000)
   selfLocation()
+  getDataZoom();
+  console.log(universities)
 }
 function aerialView(){
   clearInfoDiv();
   map.centerAndZoom(new BMap.Point(119,35),6);//初始化地图，point为中心点，缩放级别为6
-   map.setMapStyleV2({styleId: 'a5fddb25f2312728e44a8dd938e24fd2'});
 }
 function selfLocation(){
-	var geolocation = new BMap.Geolocation();
-	geolocation.getCurrentPosition(function(r){
-		if(this.getStatus() == BMAP_STATUS_SUCCESS){
-		  var marker=new BMap.Marker(r.point)
-		  current_location=new BMap.Point(r.point.lng,r.point.lat)
-		  var lab = new BMap.Label('<b>我的位置</b>')
-		  marker.setLabel(lab)
-		  map.addOverlay(marker)
-		  lab.setStyle({
-			//borderColor: "#808080",
-			borderWidth: 0,
-			color: "#ddd",
-			cusor: "pointer",
-			backgroundColor: "#ec2d2d"
-		  })
-		}
-	else {
-		current_location=new BMap.Point(120.54572,30.645852)
-	}        
-});}
+  var geolocation = new BMap.Geolocation();
+  geolocation.getCurrentPosition(function(r){
+    if(this.getStatus() == BMAP_STATUS_SUCCESS){
+      var marker=new BMap.Marker(r.point)
+      current_location=new BMap.Point(r.point.lng,r.point.lat)
+      var lab = new BMap.Label('<b>我的位置</b>')
+      marker.setLabel(lab)
+      map.addOverlay(marker)
+      lab.setStyle({
+        //borderColor: "#808080",
+        borderWidth: 0,
+        color: "#ddd",
+        cusor: "pointer",
+        backgroundColor: "#ec2d2d"
+      })
+    }
+    else {
+      current_location=new BMap.Point(120.54572,30.645852)
+    }
+  });}
 
 function setMarker(data, index){
   var point = new BMap.Point(data.Lng, data.Lat);
@@ -81,12 +83,12 @@ function setMarker(data, index){
 }
 
 function showWay(lng,lat){
-	var start = current_location
-    var end = new BMap.Point(lng,lat)
-	var transit = new BMap.TransitRoute(map, { 
-    renderOptions: { 
-        map: map, 
-        autoViewport: true
+  var start = current_location
+  var end = new BMap.Point(lng,lat)
+  var transit = new BMap.TransitRoute(map, {
+    renderOptions: {
+      map: map,
+      autoViewport: true
 
     },
 
@@ -98,19 +100,42 @@ function showWay(lng,lat){
 
     //transitTypePolicy: BMAP_TRANSIT_TYPE_POLICY_AIRPLANE
 
-});
+  });
 
 
-transit.search(start, end);
+  transit.search(start, end);
 }
 function getmylocation(){
-	try{console.log(current_location.lat)}catch(err){
-		current_location=new BMap.Point(120.54572,30.645852)
-	
-}return current_location}
+  try{console.log(current_location.lat)}catch(err){
+    current_location=new BMap.Point(120.54572,30.645852)
+
+  }return current_location}
 
 function randomOrder(){
-	
-	index_list.push(index_list.shift())
-	index_list.reverse()//暂时毫无思路，待完善
+
+  index_list.push(index_list.shift())
+  index_list.reverse()//暂时毫无思路，待完善
+}
+function getDataZoom(){
+  for (var i=0; i<universities.length; i++){
+    var p0 = new BMap.Point(universities[i].Lng, universities[i].Lat)
+    var min_dis = 300000; // JUst big enough
+    for(var j=0; j<universities.length; j++){
+      if (i == j){continue}
+      var p1 = new BMap.Point(universities[j].Lng, universities[j].Lat)
+      var dis = map.getDistance(p0, p1)
+      if (i == 15){console.log(dis)}
+      if (dis < min_dis){min_dis = dis}
+    }
+    universities[i].Zoom = Math.max(getZoom(min_dis), 6);
+    universities[i].min_dis = min_dis;
+  }
+}
+function getZoom (min_dis) {
+  var zoom = ["50","100","200","500","1000","2000","5000","10000","20000","25000","50000","100000","200000","500000","1000000","2000000"]//级别18到3。
+  for (var i = 0,zoomLen = zoom.length; i < zoomLen; i++) {
+    if(zoom[i] - min_dis > 0){
+      return 18-i;//之所以会多3，是因为地图范围常常是比例尺距离的10倍以上。所以级别会增加3。
+    }
+  };
 }
