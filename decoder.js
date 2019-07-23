ef963437bce4de2dbf40b88976a24baa8abec27d
Allan Chain <1599654796@qq.com>
2019-07-23 09:40:21 +0800

    merge index and guest into one

--- decoder.js
+++ decoder.js
@@ -6,16 +6,27 @@ function enterdecode(e){
       e=CryptoJS.enc.Latin1.stringify(d)
       f=decodeURIComponent(escape(window.atob(e)))
       eval(f)
-	  loadJScript();
-      $("#key_input").remove();
-	  $("#ctext").removeAttr("hidden")
+      clearAndLoad();
+      $("#back_main").remove()
       $(".search_box").slideToggle()
-      $("#welcome_title").remove();
-      $("#copyright").remove();
     }catch(err){
       $("#key_input").css("background-color", "red")
       $("#key").val('');
       $("#key").attr("placeholder", "密钥不匹配 | 请重新输入")
     }
   }
+}
+function goGuest(){
+  clearAndLoad()
+  $("#search_input").remove()
+  $("#search_button").remove()
+  $("#add_data").remove()
+  $(".search_box").slideToggle()
+}
+function clearAndLoad(){
+  loadJScript();
+  $("#key_input").remove();
+  $("#ctext").removeAttr("hidden")
+  $("#welcome_title").remove();
+  $("#copyright").remove();
 }
