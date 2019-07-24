97f785dcfba74010d71b48afcceacb1c596cc380
Allan Chain <1599654796@qq.com>
2019-07-24 15:34:52 +0800

    direct jump to guest mode, seems fine

--- search_parser.js
+++ search_parser.js
@@ -5,12 +5,17 @@ searchStr = searchStr.substr(1);
 //将searchStr字符串分割成数组，数组中的每一个元素为一个参数和参数值
 var searchs = searchStr.split("&");
 var classname = "TG2019303";
+var isguest = 0;
 for (var i=0; i<searchs.length; i++){
   kv = searchs[i].split("=")
   if (kv[0] == "banji"){
     classname = kv[1]
   }
+  if (kv[0] == "guest"){
+    isguest = kv[1]
+  }
 }
+$("#back_main").attr("href", "/?banji="+classname)
 var script = document.createElement("script");
 script.type = "text/javascript";
 script.src = "data/" + classname + "/dataEncrypted.js";
@@ -28,3 +33,10 @@ csslink.rel = "stylesheet";
 csslink.type = "text/css";
 csslink.href = "data/" + classname + "/main.css";
 document.body.appendChild(csslink);
+if (isguest == 1){
+  goGuest();
+}else{
+  $("#key_input").removeAttr("hidden");
+  $("#welcome_title").removeAttr("hidden");
+  $("#copyright").removeAttr("hidden");
+}
