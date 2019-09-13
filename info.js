f6a297430182e66d005d395fe0cccdd1c39bcfd1
Allan Chain <1599654796@qq.com>
2019-09-13 10:05:39 +0800

    implement slide and scroll

--- info.js
+++ info.js
@@ -3,19 +3,33 @@ var vapp = new Vue({
   data: {
     show_info_div: false,
     show_more: false
+  },
+  methods: {
+    toggleShow: function() {
+      $("#student_ul").scrollTop(0);
+      this.show_more=!this.show_more
+    },
+    scrollTo: function() {
+      var container = $("#student_ul"), target = $(".student_hilite");
+      container.scrollTop(target.offset().top-container.offset().top-30);
+    }
   }
 })
-function showInfo(data, hilite=-1){
+
+function showInfo(data, hilite = -1) {
   // I don't know why force update is needed
   vapp.show_info_div = false;
   vapp.data = data;
   vapp.show_info_div = true;
   vapp.hilite_univ = (hilite == -2);
   vapp.hilite_index = hilite;
+  if (hilite >= 0) {
+    setTimeout(vapp.scrollTo, 300)
+  }
   var UA = window.navigator.userAgent.toLocaleLowerCase();
-  if(/iphone|ipad|ipod/.test(UA)){ // ios
+  if (/iphone|ipad|ipod/.test(UA)) { // ios
     vapp.ios = true
-  }else{
+  } else {
     vapp.ios = false
   }
   vapp.my = getmylocation();
